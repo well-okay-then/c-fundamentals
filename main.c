74bf8dc9bd4db457c83eb8c9721fcbb6928b4be8
well-okay-then <ericsoppi@proton.me>
2026-09-03 21:40:09 -0400

    done

--- main.c
+++ main.c
@@ -4,23 +4,11 @@ int main(void) {
     int n;
     scanf("%d", &n);
 
-    /* Print exactly one line:
-         n divisible by 15 -> FizzBuzz
-         n divisible by 3  -> Fizz
-         n divisible by 5  -> Buzz
-         anything else     -> n itself
-
-       TODO: write the if / else if / else chain. All four cases. */
-
-    if (n % 15 == 0) {
-        printf("FizzBuzz");
-    } else if (n % 3 == 0) {
-        printf("Fizz");
-    } else if (n % 5 == 0) {
-        printf("Buzz");
-    } else {
-        printf("%d", n);
+    int total = 0;
+    for (int i = 1; i <= n; ++i) {
+        total += i;
     }
-
+    printf("%d", total);
+    // Loop and sum, then print.
     return 0;
 }
